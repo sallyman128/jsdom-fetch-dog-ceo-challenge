2d112b9db50e6e650f51100955baccacb3279852
Salmaan <salmaan.arif.ali@gmail.com>
2021-06-24 00:30:23 -0400

    commit tree e9f1fa79b946982363cc3a069edfdc23e11559dc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+addEventListener('DOMContentLoaded', function() {
+  fetch(imgUrl)
+  .then(resp => resp.json())
+  
+})
